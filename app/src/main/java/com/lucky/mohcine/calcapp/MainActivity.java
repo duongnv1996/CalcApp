66d36dd2bdd927b7cfc976f30b89801ee93db813
DESKTOP-KB19RNK\DuongKK <duongnv1996@gmail.com>
2017-11-28 16:19:27 +0700

    comit

--- app/src/main/java/com/lucky/mohcine/calcapp/MainActivity.java
+++ app/src/main/java/com/lucky/mohcine/calcapp/MainActivity.java
@@ -1,16 +1,15 @@
 package com.lucky.mohcine.calcapp;
 
-import android.media.MediaCodec;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.regex.Pattern;
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -49,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void onSetmins(View view){
         String mins= "-";
-        display += mins;
+        display = mins+display;
         updateDisplay();
 
     }
@@ -95,7 +94,6 @@ public class MainActivity extends AppCompatActivity {
     public  void clickEqual(View view){
 
 
-
         if(screen_display.getText() == "" && screen_display.getText() == currentOperator ){
             Toast.makeText(MainActivity.this , "Please entre your operation" ,Toast.LENGTH_LONG).show();
         }
@@ -104,13 +102,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public boolean onEqual() {
+//
+//        if (currentOperator == "") return false;
+//        String [] _display = display.split(Pattern.quote(currentOperator));
+//        if (_display.length<2) return false;
+//        if(currentOperator.equals("÷")) currentOperator ="/";
+//        result = String.valueOf(operator(_display[0], _display[1], currentOperator));
+//        screen_result.setText(String.valueOf(result));
 
-        if (currentOperator == "") return false;
-        String [] _display = display.split(Pattern.quote(currentOperator));
-        if (_display.length<2) return false;
-        result = String.valueOf(operator(_display[0], _display[1], currentOperator));
+        String cal =  screen_display.getText().toString();
+       cal =  cal.replace("x","*").replace("÷","/").replace("%","/100");
+
+        Expression e = new ExpressionBuilder(cal)
+                .build();
+        double result = e.evaluate();
         screen_result.setText(String.valueOf(result));
-
         return true;
     }
 
